f8b76564b141af655ec6e265a1fa66cba595cd8a
Elex03 <eacuna042@gmail.com>
2024-02-29 22:12:54 -0600

    remove type

--- src/server.js
+++ src/server.js
@@ -2,9 +2,7 @@ import express from 'express'
 import morgan from 'morgan'
 import cors from 'cors'
 
-import { getConnection } from './database/conecction'
 import {Connection} from './models/products.model'
-import { PORT } from './config/config'
 import { routes } from './routes/index'
 
 
