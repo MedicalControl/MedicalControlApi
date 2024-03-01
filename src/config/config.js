15eb5052c0844c19695052c44fd46692f09123fd
Elex03 <eacuna042@gmail.com>
2024-03-01 11:25:54 -0600

    Connection database

--- src/config/config.js
+++ src/config/config.js
@@ -5,3 +5,7 @@ if(process.env.NODE_ENV !== 'production')
 
 export const PORT = process.env.PORT;
 export const jwt = process.env.JET_SECRET || 'SecretNote'
+export const DB_USER = process.env.DB_USER;
+export const DB_PASSWORD = process.env.DB_USER;
+export const DB_HOST = process.env.DB_HOST;
+export const DB_NAME = process.env.DB_NAME;
--- src/database/conecction.js
+++ src/database/conecction.js
@@ -1,10 +1,12 @@
 import sql from 'mssql'
+import {DB_NAME, DB_PASSWORD, DB_USER, DB_HOST} from '../config/config.js'
+
 
 const dbsettings = {
-    user: "Zero",
-    password: "Zero314.",
-    server: "localhost",
-    database: "Medical_Control",
+    user: DB_USER,
+    password: DB_PASSWORD,
+    server: DB_HOST,
+    database: DB_NAME,
     options: {
         encrypt: true,
         trustServerCertificate: true,
