09682768153e683b27f50aa526beddbb7815dbf3
Elex03 <eacuna042@gmail.com>
2024-04-04 22:44:29 -0600

    change file user.controller message

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -14,7 +14,7 @@ export const loginUsers = async (req, res) => {
     if (!email || !password)
         return res.status(400).json({ msg: "The fields cannot be empty" })
     if (!userWithEmail || !bcrypt.compareSync(password, userWithEmail.password))
-        return res.status(400).json({ message: "Email or password does not match!" });
+        return res.status(400).json({ msg: "Email or password does not match!" });
     const jwtToken = jwt.sign({ id: userWithEmail.id, email: userWithEmail.email }, jwtSK);
     res.status(201).json({ token: jwtToken })
 }
