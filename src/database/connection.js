1011b3c9d8c7a091d776fae23ee6027a77d6a827
Elex03 <eacuna042@gmail.com>
2024-05-05 18:09:54 -0600

    fix connection

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,8 +1,18 @@
 import { Sequelize } from 'sequelize';
 import { DATABASE_URI} from '../config/config.js'
 
-export const pool = new Sequelize(DATABASE_URI);
-
+export const pool = new Sequelize(
+    DATABASE_URI,{
+        dialect: 'postgres', 
+        logging: false,
+        dialectOptions:{
+            ssl:{
+                require: true, 
+                rejectUnauthorized: false
+            }
+        }
+    }
+);
 
 export async function getConnection() {
     pool.sync().then(() => {
